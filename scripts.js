a21b2c5b08bd46e61b834e51ec0e4cd783407891
hermanjor <herman.jorstad@outlook.com>
2021-10-05 15:07:34 +0200

    Visningsendringer og ryddet kode

--- scripts.js
+++ scripts.js
@@ -2,15 +2,14 @@
 
 //Kontaktskjema her kanskje?
 
-
 function visHistorie() {
-    var x = document.getElementById("historie");
-    var y = document.getElementById("historieKnapp");
-    if (x.style.display === "none") {
-      x.style.display = "block";
-      y.innerText = "X";
-    } else {
-      x.style.display = "none";
-      y.innerText = "Jeg vil vite mer!";
-    }
+  var x = document.getElementById("historie");
+  var y = document.getElementById("historieKnapp");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    y.innerText = "Skjul teksten↑";
+  } else {
+    x.style.display = "none";
+    y.innerText = "Jeg vil vite mer!";
   }
+}
